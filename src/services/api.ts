021044e8711e3a512d7fe3bfc3f248c36757749e
Hitalo Rodrigo Alves <hitalo.ralves@outlook.com>
2022-01-10 07:23:08 -0300

    Update baseUrl config axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL:'http://hitaloalvess-dtmoney.herokuapp.com/'
+    baseURL:'https://hitaloalvess-dtmoney.herokuapp.com/'
 })
